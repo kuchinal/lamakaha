31cfc05fc519979614ed40798841839013ae3473
kuchinal <lamakaha@gmail.com>
2019-05-05 21:12:30 +0200

    first work commit

--- init.py
+++ init.py
@@ -4,5 +4,4 @@ nuke.pluginAddPath(UserDir+"Folders/Icons/")
 nuke.pluginAddPath(UserDir+"Folders/Gizmos/")
 nuke.pluginAddPath(UserDir+"Folders/Plugins/")
 nuke.pluginAddPath(UserDir+"Folders/NukeScripts/")
-#test
 
---
+++ some_hints.py
@@ -0,0 +1,6 @@
+commit to local:			"git commit -m 'setting up proper branch'"
+stage files for commit:		"git add -A"
+shot current status:		"git status"
+pull files from github : 	"git pull https://github.com/kuchinal/lamakaha master"
+push files to github: 		"git push https://github.com/kuchinal/lamakaha master"
+
