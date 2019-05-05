7626e4acf22c07b8167513ea96a469204033691f
kuchinal <lamakaha@gmail.com>
2019-05-05 20:34:34 +0200

    initial commit

---
+++ init.py
@@ -0,0 +1,7 @@
+
+nuke.pluginAddPath(UserDir+"Folders/Python/")
+nuke.pluginAddPath(UserDir+"Folders/Icons/")
+nuke.pluginAddPath(UserDir+"Folders/Gizmos/")
+nuke.pluginAddPath(UserDir+"Folders/Plugins/")
+nuke.pluginAddPath(UserDir+"Folders/NukeScripts/")
+
