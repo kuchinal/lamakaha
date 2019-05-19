dff01aecd2176ee78d54a4d7a47bb41f82cdf3d3
kuchinal <lamakaha@gmail.com>
2019-05-19 22:01:38 +0200

    seemsLikeAllIn

--- Folders/Python/Black.py
+++ Folders/Python/Black.py
@@ -5,3 +5,12 @@
     b['tile_color'].setValue(1)
     b['label'].setValue("Black Alpha")   
     print "hallo"
+
+
+
+def White():
+    import nuke
+    b = nuke.createNode("Shuffle")
+    b['alpha'].setValue(6)
+    b['tile_color'].setValue(4294967295)
+    b['label'].setValue("White Alpha")  
