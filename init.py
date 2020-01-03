1991990ecd565758eff5e6dd3b9d8613d2ed74c0
kuchinal <lamakaha@gmail.com>
2020-01-03 22:48:44 +0100

    all

--- init.py
+++ init.py
@@ -10,3 +10,7 @@ nuke.pluginAddPath("Folders/NukeScripts/")
 nuke.pluginAddPath("Folders/Python/KnobScripter/")
 nuke.pluginAddPath("Folders/Python/MassivePanel/")
 
+
+
+
+nuke.pluginAddPath("Folders/Python/id3-py-1.2/")
