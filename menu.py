9475a4edc71203e66687f6a23696af13f6fe22d2
lamakaha <lamakaha@gmail.com>
2023-08-20 22:45:55 +0200

    some cleanup

--- menu.py
+++ menu.py
@@ -153,7 +153,7 @@ nodesMenu.addCommand("Tracker", "nuke.createNode(\"Tracker4\")", "ctrl+t",icon="
 
 nodesMenu.addCommand("Transform/CardToTrack/CardToTrack", 'nuke.createNode(\'CardToTrack\')',icon='my.png')
 nodesMenu.addCommand("Transform/CardToTrack/CProject", 'nuke.createNode(\'CProject\')',icon='CornerPin.png')
-nodesMenu.addCommand("Transform/CardToTrack/TProject", 'nuke.createNode(\'CProject\')',icon='Transform.png')
+nodesMenu.addCommand("Transform/CardToTrack/TProject", 'nuke.createNode(\'TProject\')',icon='Transform.png')
 
 nodesMenu.addCommand('Nodegraph/Auto Backdrop', 'import autoBackdropp; autoBackdropp.autoBackdrop()', 'alt+b',icon = "Backdrop.png")
 nodesMenu.addCommand('Nodegraph/Auto Sticky', 'import autoSticky; autoSticky.autoSticky()', 'alt+n',icon = "Backdrop.png")
